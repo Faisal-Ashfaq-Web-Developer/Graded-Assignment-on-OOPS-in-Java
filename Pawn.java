ad1cd7b5e93bc5388874892956d98a66b71cba52
Faisal Ashfaq <faisal0ashfaq@gmail.com>
2024-01-07 05:16:14 +0530

    Add files via upload

---
+++ Pawn.java
@@ -0,0 +1,8 @@
+public class Pawn extends Piece {
+
+    public Pawn(boolean available, int x, int y) {
+        super(available, x, y);
+    }
+
+    // Additional methods for Pawn can be added here
+}
